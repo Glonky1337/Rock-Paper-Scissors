e472d35f6b88d3e6920d9d70764ad3c5c2801c62
Glonky1337 <fergusontbs@gmail.com>
2023-02-20 15:02:06 -0500

    Create initial pseudocode for structure of application

---
+++ app.js
@@ -0,0 +1,12 @@
+// Create function to get player's input of Rock, Paper, or Scissor
+// Prompt player to input either Rock, Paper or Scissor
+// Make player's selection case-insensitive
+// Return value for variable
+
+// Create function to generate computer's input
+// Return value for variable
+
+// Create function to compare both selection parameters, and announce winner
+// Increment winner's score by one for each round
+
+// Create a function that play's five rounds, and announces the winner of the game.
