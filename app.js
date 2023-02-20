4f9c3ede158a8465e8bfadd1feeb69fae4ebd3e4
Glonky1337 <fergusontbs@gmail.com>
2023-02-20 16:06:03 -0500

    Create function to randomly generate computer's selection

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ function getPlayerChoice (){ // Create function to get player's input of Rock, P
     playerSelection = playerSelection.toLowerCase(); // Make player's selection case-insensitive
     return playerSelection; //return the value for playerSelection to Global Scope
 }
-const playerSelection = getPlayerChoice(); // declare constant variable based on the getPlayerChoice() function.
+const playerSelection = getPlayerChoice(); // declare constant variable based on the getPlayerChoice() function
+console.log(playerSelection);
+
+function generateComputerInput() { // Create function to generate computer's input
+    const options = ['rock', 'paper', 'scissors']; // Create array that contains the options rock, paper, or scissors
+    const randomIndex = Math.floor(Math.random() * options.length); // Declare const that randomly generates/rounds a num based on length of options array.
+    let computerSelection = options[randomIndex]; // Select and assign corresponding value from options array
+    return computerSelection; // return the value for computerSelection to Global Scope
+}
+const computerSelection = generateComputerInput(); // declare constant variable based on the generateComputerInput() function
+console.log(computerSelection);
+
 
 // Create function to generate computer's input
 // Return value for variable
