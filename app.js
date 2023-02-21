057535f130e58b5e7f07e3a1e0ce062a5c7cc6cb
Glonky1337 <fergusontbs@gmail.com>
2023-02-20 19:47:10 -0500

    Remove duplicate comments at end of file

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function getPlayerChoice (){ // Create function to get player's input of Rock, Paper, or Scissor
+function getPlayerChoice (){ // Get player's input of Rock, Paper, or Scissor
     let playerSelection = prompt('Rock, Paper, or Scissors?'); // Prompt player to input either Rock, Paper, or Scissor
     playerSelection = playerSelection.toLowerCase(); // Make player's selection case-insensitive
     return playerSelection; // Return the value for playerSelection to Global Scope
 }
 
-function generateComputerInput() { // Create function to generate computer's input
+function generateComputerInput() { // Generate computer's input
     const options = ['rock', 'paper', 'scissors']; // Create array that contains the options rock, paper, or scissors
     const randomIndex = Math.floor(Math.random() * options.length); // Declare const that randomly generates/rounds a num based on length of options array.
     let computerSelection = options[randomIndex]; // Select and assign corresponding value from options array
-    return computerSelection; // return the value for computerSelection to Global Scope
+    return computerSelection; // Return the value for computerSelection to Global Scope
 }
 
-function determineRoundWinner(playerSelection, computerSelection) { // Create function to compare both selection parameters, and announce Winner
+function determineRoundWinner(playerSelection, computerSelection) { // Compare both selection parameters, and announce Winner
     let playerScore = 0;
     let computerScore = 0;
     
@@ -41,20 +41,20 @@ function determineRoundWinner(playerSelection, computerSelection) { // Create fu
     return {playerScore, computerScore};
 }
 
-function game() {
-    let playerTotalScore = 0;
-    let computerTotalScore = 0;
+function game() {  // Play five rounds, and announce the winner of the game
+    let playerTotalScore = 0; // start each round with score of 0
+    let computerTotalScore = 0; // start each round with score of 0
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) { // loop for five rounds
         const playerSelection = getPlayerChoice();
         const computerSelection = generateComputerInput();
         const result = determineRoundWinner(playerSelection, computerSelection);
-        playerTotalScore += result.playerScore;
-        computerTotalScore += result.computerScore;
+        playerTotalScore += result.playerScore; // increment winner's score by one for each win
+        computerTotalScore += result.computerScore; // increment winner's score by one for each win
         console.log(`Player Score: ${playerTotalScore}`);
         console.log(`Computer Score: ${computerTotalScore}`);
     }
-    
+
     if (playerTotalScore > computerTotalScore) {
         console.log('Congratulations, you won!');
     } else if (playerTotalScore < computerTotalScore) {
@@ -64,8 +64,4 @@ function game() {
     }
 }
 
-game();
-
-// Increment winner's score by one for each round
-
-// Create a function that play's five rounds, and announces the winner of the game.
+game(); // execute game
